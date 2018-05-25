aef5394412e42e0c99a48bb534bf277576015862
Harish S. Bhat <hbhat@ucmerced.edu>
2018-05-24 22:56:37 -0700

    Forgot setup.py earlier

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='bridgeEM',
+      version='1.0',
+      description='Diffusion Bridge EM',
+      author='Shagun Rawat',
+      author_email='shagunrawat27@gmail.com',
+      packages=['bridgeEM'],
+     )
+

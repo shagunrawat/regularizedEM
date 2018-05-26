Added precision/recall metric and lasso regularization comparison. Need to correct it and use grouped lasso.

bridgeEM/em_functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from joblib import Parallel, delayed
+from sklearn import linear_model
 
 # drift function using "basis" functions defined by mypoly
 # x must be a numpy array, the points at which the drift is to be evaluated
@@ -53,9 +54,11 @@ def girsanov(d_param, em_param, path, tdiff):
     r = int1 - 0.5 * int2
     return r
 
-def lasso_regularization(theta, x, threshold):
-    y = np.transpose(np.diff(x, axis = 0))
+def comparison(x, threshold, method_type = 'lasso'):
+    y = np.diff(x, axis = 0)
     M = hermite_basis(x[:(-1)])
+    clf = linear_model.Lasso(alpha = threshold)
+    clf.fit(M, y)
     return theta
 
 # this function computes MCMC steps for i-th interval of the j-th time series
@@ -102,10 +105,10 @@ def mcmc(allx, allt, d_param, em_param, path_index, step_index):
             arsamp[jj] = 1
 
         samples = xcur
-        pp = hermite_basis(samples[:(-1)])
-        mmat = mmat + tdiff * np.matmul(pp.T, pp) / em_param.mcmcpaths
-        rvec = rvec + np.matmul((np.diff(samples, axis = 0)).T, pp) / em_param.mcmcpaths   
-        gammavec = gammavec + np.sum(np.square(np.diff(samples, axis = 0) - tdiff * np.matmul(pp, d_param.theta)), axis = 0) / (tdiff * em_param.mcmcpaths * (em_param.numsubintervals * (allx.shape[1] - 1) + 1))
+        Hmat = hermite_basis(samples[:(-1)])
+        mmat = mmat + tdiff * np.matmul(Hmat.T, Hmat) / em_param.mcmcpaths
+        rvec = rvec + np.matmul((np.diff(samples, axis = 0)).T, Hmat) / em_param.mcmcpaths   
+        gammavec = gammavec + np.sum(np.square(np.diff(samples, axis = 0) - tdiff * np.matmul(Hmat, d_param.theta)), axis = 0) / (tdiff * em_param.mcmcpaths * em_param.numsubintervals * (allx.shape[1] - 1))
     meanSample = np.mean(arsamp)
     
     return (mmat, rvec, gammavec, meanBurnin, meanSample, path_index, step_index)
@@ -146,6 +149,7 @@ def exp_max(allx, allt, em_param, d_param, reg_param):
         # all other beta components fixed. This is iterated till convergence is reached
         if (reg_param.reg_type == 'soft'):
             reg_theta = lasso_regularization(theta = newtheta, x = allx, threshold = reg_param.threshold)
+
         if (reg_param.reg_type == 'hard'):
             reg_theta = newtheta
             reg_theta[np.abs(reg_theta) < reg_param.threshold] = 0.

bridgeEM/polynomial_functions.py
@@ -2,16 +2,16 @@ import numpy as np
 
 # defines polynomial basis functions {1, x, x^2, x^3}
 def polynomial_basis(x):
-    theta = np.full((x.shape[0], dof.dof), 1.)
+    theta = np.full((x.shape[0], ddd.dof), 1.)
     index_map = index_mapping()
 
     for index in index_map:
-        for d in range(dof.dim):
+        for d in range(ddd.dim):
             theta[:, index_map[index]] *= np.power(x[:, d], index[d])
 
     return theta
 
-def H(degree, x):
+def H(x, degree):
     switcher = {
         0: 0.63161877774606470129,
         1: 0.63161877774606470129 * x,
@@ -25,33 +25,18 @@ def H(degree, x):
 # x must be a numpy array, a column vector of points
 # (x = vector of points at which we seek to evaluate the basis functions)
 # dof is the number of degrees of freedom, i.e., the number of basis functions.
-
-# TODO : currently this loop has to be written separately for varying dimensions.
 def hermite_basis(x):
-    theta = np.full((x.shape[0], dof.dof), 1.)
+    theta = np.full((x.shape[0], ddd.dof), 1.)
     index_map = index_mapping()
 
     for index in index_map:
-        for d in range(dof.dim):
-            theta[:, index_map[index]] *= H(index[d], x[:, d])
+        for d in range(ddd.dim):
+            theta[:, index_map[index]] *= np.H(x[:, d], index[d])
 
     return theta
 
-def index_mapping():
-    index = 0
-    index_map = {}
-
-    for d in range(0, dof.degree):
-        for i in range(0, d + 1):
-            if (i == d):
-                index_set = (i)
-                index_map[index_set] = index
-                index += 1
-
-    return index_map
-
 def h2o_simple_transformation():
-    mat = np.zeros((dof.degree, dof.degree))
+    mat = np.zeros((ddd.terms, ddd.terms))
     mat[0, 0] = 0.63161877774606470129
     mat[1, 1] = 0.63161877774606470129
     mat[2, 2] = 0.44662192086900116570
@@ -61,24 +46,18 @@ def h2o_simple_transformation():
 
     return mat
 
+# for the dim-dimensional, terms-hermite terms case, creating the transformation matrix for
+# any index mapping provided
 def h2o_transformation_matrix():
-    transformation = np.zeros((dof.dof, dof.dof))
+    transformation = np.full((ddd.dof, ddd.dof), 1.)
     index_map = index_mapping()
-    index = 0
-
     mat = h2o_simple_transformation()
 
-    for d in range(0, dof.degree):
-        for i in range(0, d + 1):
-            if (i == d):
-                transformation[index, index] = mat[i, i]
-                if (i >= 2):
-                    new_index_set = (i - 2)
-                    new_index = index_map[new_index_set]
-                    transformation[new_index, index] = mat[i - 2, i]
-
-                index += 1
-
+    for row_index in index_map:
+        for col_index in index_map:
+            for d in range(ddd.dim):
+                transformation[index_map[row_index], index_map[col_index]] *= mat[row_index[d], col_index[d]]
+                
     return transformation
 
 def hermite_to_ordinary(theta):
@@ -91,24 +70,36 @@ def ordinary_to_hermite(theta):
     hermite_theta = np.matmul(transformation, theta)
     return hermite_theta
 
-def norm_error(true_theta, estimated_theta):
+def compute_error(estimated, true, errors_computed):
     errors = []
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.ordinary - estimated_theta.ordinary), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.hermite - estimated_theta.hermite), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.sparse_ordinary - estimated_theta.sparse_ordinary), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.sparse_hermite - estimated_theta.sparse_hermite), 2))))
-    return errors
 
-def theta_sparsity(theta):
-    threshold = 0.1 * np.max(np.abs(theta))
-    theta[np.abs(theta) < threshold] = 0.
-    return theta
+    # regression metric
+    # L1 norm
+    if (errors_computed[0]):
+        errors.append(np.sum(np.abs(true - estimated)))
 
-def compute_error(estimated, true, type = 'L2norm'):
-    if (type = 'L2norm'):
-        return (np.sqrt(np.sum(np.power(np.abs(true_theta.ordinary - estimated_theta.ordinary), 2))))
+    # L2 norm
+    if (errors_computed[1]):
+        errors.append(np.sqrt(np.sum(np.power(true - estimated, 2))))
 
-    if (type = 'precision'):
+    # classification metric, P = value is zero, N = value is non-zero
+    # true positive => true was zero and estimated was zero
+    TP = np.sum(np.logical_and(true == 0., estimated == 0.))
+    # true negative => true was non-zero and estimated was non-zero
+    TN = np.sum(np.logical_and(true != 0., estimated != 0.))
+    # false positive => true was non-zero and estimated was zero
+    FP = np.sum(np.logical_and(true != 0., estimated == 0.))
+    # false negative => true was zero and estimated was non-zero
+    FN = np.sum(np.logical_and(true == 0., estimated != 0.))
 
-    if (type = 'recall'):
+    # precision = true positives / total estimated positives {TP / (TP + FP)}
+    if (errors_computed[2]):
+        errors.append(TP / (TP + FP))
+
+    # recall = true positives / total true positives {TP / (TP + FN)}
+    if (errors_computed[3]):
+        errors.append(TP / (TP + FN))
         
+    # accuracy = total true / total predictions {(TP + TN) / (TP + TN + FP + FN)}
+    if (errors_computed[4]):
+        errors.append((TP + TN) / (TP + TN + FP + FN))

build/lib/bridgeEM/em_functions.py
@@ -43,23 +43,19 @@ def brownianbridge(d_param, em_param, xin, tin):
 
 # Girsanov likelihood is computed using # TODO: insert reference to the paper
 def girsanov(d_param, em_param, path, tdiff):
-	# path is of size (numsubintervals + 1, dim)
-	# b is of size (numsubintervals + 1, dim)
-	b = drift(d_param, path)
-	u = np.dot(np.diag(np.power(d_param.gvec, -2)), b.T).T
-	int1 = np.tensordot(u[:-1, :], np.diff(path, axis = 0))
-	u2 = np.einsum('ij,ji->i', u.T, b)
-	int2 = np.sum(0.5 * (u2[1:] + u2[:-1])) * (tdiff)
-	r = int1 - 0.5 * int2
-	return r
+    # path is of size (numsubintervals + 1, dim)
+    # b is of size (numsubintervals + 1, dim)
+    b = drift(d_param, path)
+    u = np.dot(np.diag(np.power(d_param.gvec, -2)), b.T).T
+    int1 = np.tensordot(u[:-1, :], np.diff(path, axis = 0))
+    u2 = np.einsum('ij,ji->i', u.T, b)
+    int2 = np.sum(0.5 * (u2[1:] + u2[:-1])) * (tdiff)
+    r = int1 - 0.5 * int2
+    return r
 
-def lasso_reg(theta, threshold, type):
-    if (type == 'soft'):
-
-
-    if (type == 'hard'):
-        theta[np.abs(theta) < threshold] = 0.
-        
+def lasso_regularization(theta, x, threshold):
+    y = np.transpose(np.diff(x, axis = 0))
+    M = hermite_basis(x[:(-1)])
     return theta
 
 # this function computes MCMC steps for i-th interval of the j-th time series
@@ -119,7 +115,7 @@ def mcmc(allx, allt, d_param, em_param, path_index, step_index):
 # to get the next iteration of theta (M-step). The function returns successfully if the
 # absolute error goes below specified tolerance. The function returns unsuccessfully if the
 # number of M-step iterations go beyond a threshold without reducing the error below tolerance.
-def exp_max(allx, allt, em_param, d_param, reg_threshold):
+def exp_max(allx, allt, em_param, d_param, reg_param):
     done = False
     numiter = 0
     error_list = []
@@ -144,16 +140,21 @@ def exp_max(allx, allt, em_param, d_param, reg_threshold):
 
         newtheta = np.linalg.solve(mmat, rvec).T
 
-        # relative error
-        error = np.sum(np.abs(newtheta - d_param.theta)) / np.sum(np.abs(d_param.theta))
-
         # inducing sparsity in the Hermite space
         # type = {soft, hard} thresholding
         # the soft thresholding is an iterative process where each beta parameter gets updated while keeping
         # all other beta components fixed. This is iterated till convergence is reached
-        d_param.theta = lasso_regularization(theta = newtheta, reg_threshold, type = 'soft')
+        if (reg_param.reg_type == 'soft'):
+            reg_theta = lasso_regularization(theta = newtheta, x = allx, threshold = reg_param.threshold)
+        if (reg_param.reg_type == 'hard'):
+            reg_theta = newtheta
+            reg_theta[np.abs(reg_theta) < reg_param.threshold] = 0.
 
+        # relative error
+        error = np.sum(np.abs(reg_theta - d_param.theta)) / np.sum(np.abs(d_param.theta))
         error_list.append(error)
+
+        d_param.theta = reg_theta
         theta_list.append(d_param.theta)
         # if error is below tolerance, EM has converged
         if (error < em_param.tol):

build/lib/bridgeEM/polynomial_functions.py
@@ -2,11 +2,11 @@ import numpy as np
 
 # defines polynomial basis functions {1, x, x^2, x^3}
 def polynomial_basis(x):
-    theta = np.full((x.shape[0], dof.dof), 1.)
+    theta = np.full((x.shape[0], ddd.dof), 1.)
     index_map = index_mapping()
 
     for index in index_map:
-        for d in range(dof.dim):
+        for d in range(ddd.dim):
             theta[:, index_map[index]] *= np.power(x[:, d], index[d])
 
     return theta
@@ -25,30 +25,15 @@ def H(degree, x):
 # x must be a numpy array, a column vector of points
 # (x = vector of points at which we seek to evaluate the basis functions)
 # dof is the number of degrees of freedom, i.e., the number of basis functions.
-
-# TODO : currently this loop has to be written separately for varying dimensions.
 def hermite_basis(x):
-    theta = np.full((x.shape[0], dof.dof), 1.)
+    theta = np.full((x.shape[0], ddd.dof), 1.)
     index_map = index_mapping()
 
     for index in index_map:
-        for d in range(dof.dim):
+        for d in range(ddd.dim):
             theta[:, index_map[index]] *= H(index[d], x[:, d])
 
     return theta
-
-def index_mapping():
-    index = 0
-    index_map = {}
-
-    for d in range(0, dof.degree):
-        for i in range(0, d + 1):
-            if (i == d):
-                index_set = (i)
-                index_map[index_set] = index
-                index += 1
-
-    return index_map
 
 def h2o_simple_transformation():
     mat = np.zeros((dof.degree, dof.degree))
@@ -62,22 +47,14 @@ def h2o_simple_transformation():
     return mat
 
 def h2o_transformation_matrix():
-    transformation = np.zeros((dof.dof, dof.dof))
+    transformation = np.zeros((ddd.dof, ddd.dof))
     index_map = index_mapping()
     index = 0
 
     mat = h2o_simple_transformation()
 
-    for d in range(0, dof.degree):
-        for i in range(0, d + 1):
-            if (i == d):
-                transformation[index, index] = mat[i, i]
-                if (i >= 2):
-                    new_index_set = (i - 2)
-                    new_index = index_map[new_index_set]
-                    transformation[new_index, index] = mat[i - 2, i]
+    for index in index_map:
 
-                index += 1
 
     return transformation
 
@@ -91,16 +68,11 @@ def ordinary_to_hermite(theta):
     hermite_theta = np.matmul(transformation, theta)
     return hermite_theta
 
-def norm_error(true_theta, estimated_theta):
-    errors = []
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.ordinary - estimated_theta.ordinary), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.hermite - estimated_theta.hermite), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.sparse_ordinary - estimated_theta.sparse_ordinary), 2))))
-    errors.append(np.sqrt(np.sum(np.power(np.abs(true_theta.sparse_hermite - estimated_theta.sparse_hermite), 2))))
-    return errors
+def compute_error(estimated, true, type = 'L2norm'):
+    if (type = 'L2norm'):
+        return (np.sqrt(np.sum(np.power(np.abs(true - estimated), 2))))
 
-def theta_sparsity(theta):
-    threshold = 0.1 * np.max(np.abs(theta))
-    theta[np.abs(theta) < threshold] = 0.
-    return theta
+    if (type = 'precision'):
 
+    if (type = 'recall'):
+        